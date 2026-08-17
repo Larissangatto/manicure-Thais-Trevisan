4bbacef39b85193fdabc8d0d4fcc375eae3ded67
Larissa Nathanna Gatto <larissangatto@gmail.com>
2026-08-17 16:54:36 -0400

    Imagens carregas do google Drive

--- assets/js/trabalhos.js
+++ assets/js/trabalhos.js
@@ -1,33 +1,20 @@
+window.receberImagens = function (imagens) {
 
-console.log("teste")
-const URL_API = "https://script.google.com/macros/s/AKfycbzTBrqOEUdfxo3--Ec9BevHEAUYH0fdXsfGKFPkY-2nxKAn93dsdLO-HkRxT88eBCXt/exec"
+    const painel = document.getElementById("painel");
 
+    let newHtml = "";
 
-function receberImagens(imagens) {
+    imagens.forEach(imagem => {
 
-  
-    const elementosImg =
-        document.querySelectorAll(".foto-trabalho");
-
-    elementosImg.forEach((img, index) => {
-
-        if (imagens[index]) {
-
-            img.src = imagens[index].imagem;
-            img.alt = imagens[index].nome;
-
-        }
+        newHtml += `
+            <div class="moldura">
+                <img src="${imagem.imagem}" alt="${imagem.nome}">
+            </div>
+        `;
 
     });
-}
 
+    painel.innerHTML = newHtml;
 
-
-const script = document.createElement("script");
-
-script.src =
-    URL_API + "?callback=receberImagens";
-
-document.body.appendChild(script);
-
+};
 
